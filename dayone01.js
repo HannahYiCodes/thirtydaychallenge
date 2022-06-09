1fdc3f316b33191765e3fa0e0c5fc70126128e24
Hannah Yi <hannahyi@Hannahs-MBP.fios-router.home>
2022-06-09 18:50:23 -0400

    first day

---
+++ dayone01.js
@@ -0,0 +1,6 @@
+function processData(inputString) {
+    // This line of code prints the first line of output
+    console.log("Hello, World.");
+    console.log("Welcome to 30 Days of Code!");
+    // Write the second line of output that prints the contents of 'inputString' here.
+}
